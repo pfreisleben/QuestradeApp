0cc9f23916da5652fb1ab27edf0cb3498f261590
Pedro Freisleben <pedrofreislebeen@gmail.com>
2023-07-09 10:07:37 -0300

    add base class to dispatch domain events

--- src/Services/ScoreApi/ScoreApi.Infrastructure/Persistence/AppDb/ApplicationDbContext.cs
+++ src/Services/ScoreApi/ScoreApi.Infrastructure/Persistence/AppDb/ApplicationDbContext.cs
@@ -1,18 +1,30 @@
 ﻿using System.Reflection;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using ScoreApi.Infrastructure.Extensions;
 
 namespace ScoreApi.Infrastructure.Persistence.AppDb
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        private readonly IMediator _mediator;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IMediator mediator) : base(options)
         {
+            _mediator = mediator;
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             base.OnModelCreating(builder);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            await _mediator.DispatchDomainEventsAsync(this);
+
+            return await base.SaveChangesAsync();
         }
     }
 }
